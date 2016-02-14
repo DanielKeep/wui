da164d43a8de4dbab926c2b35f032983f88b03c1
Daniel Keep <daniel.keep@gmail.com>
2016-02-14 16:18:28 +1100

    Fixed bug in text_out.

--- src/text.rs
+++ src/text.rs
@@ -13,7 +13,7 @@ pub fn text_out<Dc>(dc: Dc, x_start: i32, y_start: i32, string: &str) -> io::Res
 where Dc: AsRaw<Raw=HDC> {
     unsafe {
         let dc = dc.as_raw();
-        let string = string.to_wide_null();
+        let string = string.to_wide();
         let ch_string = string.len().value_as::<INT>().unwrap_or_saturate();
         let string = string.as_ptr();
         match TextOutW(dc, x_start, y_start, string, ch_string) {
